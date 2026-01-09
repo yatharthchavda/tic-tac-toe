8f2f09a8b2da3c7a808cc70ec59d27da07e9ec78
yatharthchavda <yatharth.9199@gmail.com>
2026-01-09 19:45:23 +0530

    socker connection url fix

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
+// In production, connect to the same origin. In development, use env variable
+const SERVER_URL = import.meta.env.MODE === 'production' 
+  ? window.location.origin 
+  : (import.meta.env.VITE_SERVER_URL || 'http://localhost:5000');
 
 export const socket = io(SERVER_URL, {
   autoConnect: false,
